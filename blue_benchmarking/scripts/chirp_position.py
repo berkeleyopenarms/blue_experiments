d8ee5da0ded3bf15b1970f64d61593f8c4736169
Brent Yi <brentyi@berkeley.edu>
2018-11-07 01:07:09 -0600

    benchmark position pitch test

---
+++ blue_benchmarking/scripts/chirp_position.py
@@ -0,0 +1,30 @@
+import rospy
+import sys
+from std_msgs.msg import Float64MultiArray
+import numpy as np
+import scipy
+import time
+
+def p():
+        rospy.init_node('p')
+        pub = rospy.Publisher("/blue_controllers/joint_position_controller/command", Float64MultiArray, queue_size=1)
+        rate = rospy.Rate(2)
+        count = 0
+        while not rospy.is_shutdown():
+                cmd = Float64MultiArray()
+                cmd.data = [1, 0]
+                pub.publish(cmd)
+                rate.sleep()
+
+                cmd = Float64MultiArray()
+                cmd.data = [0, 0]
+                pub.publish(cmd)
+                rate.sleep()
+                count += 1
+                rospy.logerr("count = {}".format(count))
+
+if __name__ == '__main__':
+        try:
+                p()
+        except rospy.ROSInteruptException:
+                pass
