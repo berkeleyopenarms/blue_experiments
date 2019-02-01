dad42afd7b5cfc50196abe1ee171ca68bbb5640c
Brent Yi <brentyi@berkeley.edu>
2019-02-01 17:59:25 -0600

    single link testing

--- blue_benchmarking/scripts/inf_position_benchmark_test.py
+++ blue_benchmarking/scripts/inf_position_benchmark_test.py
@@ -7,19 +7,22 @@ import numpy as np
 import scipy
 import time
 
+amplitude = 0.2
+half_rate = 1
+
 def p():
         rospy.init_node('p')
         pub = rospy.Publisher("/blue_controllers/joint_position_controller/command", Float64MultiArray, queue_size=1)
-        rate = rospy.Rate(2)
+        rate = rospy.Rate(1/half_rate)
         count = 0
         while not rospy.is_shutdown():
                 cmd = Float64MultiArray()
-                cmd.data = [1, 0]
+                cmd.data = [-amplitude, 0]
                 pub.publish(cmd)
                 rate.sleep()
 
                 cmd = Float64MultiArray()
-                cmd.data = [0, 0]
+                cmd.data = [amplitude, 0]
                 pub.publish(cmd)
                 rate.sleep()
                 count += 1
